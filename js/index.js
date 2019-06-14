e7547be180a4bf05d3d8e199789b25b5a37c2f3e
OwO_OwO <31012666+OwO-OwO@users.noreply.github.com>
2019-06-13 22:48:06 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,5 +3,4 @@ $('.navTrigger').click(function () {
     console.log("Clicked menu");
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
-$('h5 span:first-child').after("<span class=\"dots\"> </span>");
 });
